f5aa0f8258ae2094818f2ed99ffd3f227c2b6980
florianGierlichs <gierlichs@gmx.de>
2020-03-26 14:19:22 +0100

    Add delete request

--- server.js
+++ server.js
@@ -88,7 +88,24 @@ app
 
   // DELETE
   .delete((req, res) => {
-    // User.findByIdAndDelete()
+    User.findByIdAndDelete(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found"
+        });
+      } else {
+        res.json({
+          success: true,
+          data: data
+        });
+      }
+    });
   });
 
 const User = require("./models/User");
